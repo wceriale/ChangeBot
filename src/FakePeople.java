43791f665fc2cf2cf3fa04a21c3228f5dfe88ffd
William <wceriale@cs.washington.edu>
2015-11-25 05:20:18 -0800

    Optimized number of threads.# OF threads should = number of available processors to maximize speed.

--- src/FakePeople.java
+++ src/FakePeople.java
@@ -26,94 +26,73 @@ import com.gargoylesoftware.htmlunit.html.HtmlHeading3;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 
+public class FakePeople extends Thread {
 
-public  class FakePeople extends Thread {
-	
 	int numberOfPeople;
 	static int count;
-	//static List<Person> listOfFakePeople;
-	
+	static List<Person> listOfFakePeople;
+	static boolean done;
+
 	public FakePeople(int numb) {
-		this.numberOfPeople = numb;	
+		this.numberOfPeople = numb;
+		listOfFakePeople = new ArrayList<Person>();
+		done = false;
 	}
-	
+
 	public void run() {
-//		for(int i = 0; i < lines.length; i++) {
-//			System.out.println(lines[i]);
-//		}
-//		System.out.print(this.getName() + " is done");
-//		this.interrupt();
-		
+		// for(int i = 0; i < lines.length; i++) {
+		// System.out.println(lines[i]);
+		// }
+		// System.out.print(this.getName() + " is done");
+		// this.interrupt();
+
 		WebClient webClient = new WebClient();
 		webClient.getOptions().setThrowExceptionOnScriptError(false);
 		HtmlPage page;
 		List<Person> people = new ArrayList<Person>();
 		try {
 			page = webClient.getPage("http://www.fakenamegenerator.com");
-			for(int i = 0; i < this.numberOfPeople; i++) { 
+			for (int i = 0; i < this.numberOfPeople; i++) {
 				HtmlHeading3 webName = (HtmlHeading3) page.getByXPath("//div[@class='info']//div//div//h3").get(0);
-		        String name = webName.getTextContent().trim();
-		        String[] fullName = name.split("\\s+");
-		        
-		        String fName = fullName[0];
-		        String lName = fullName[fullName.length - 1];
-		        
-		        
+				String name = webName.getTextContent().trim();
+				String[] fullName = name.split("\\s+");
+
+				String fName = fullName[0];
+				String lName = fullName[fullName.length - 1];
+
 				HtmlDivision webAdd = (HtmlDivision) page.getByXPath("//div[@class='info']//div//div//div").get(0);
 				String fullAddress = webAdd.getTextContent().trim();
-				
+
 				String[] splitAdd = fullAddress.split(",");
 				String[] splitAddress = splitAdd[0].trim().split("\\s+");
 				String address = splitAddress[0];
 				int index = 1;
-				while(!containsUpp(splitAddress[index])) {
+				while (!containsUpp(splitAddress[index])) {
 					address += " " + splitAddress[index];
 					index++;
 				}
 				address += " " + cutAdd(splitAddress[splitAddress.length - 1]);
-				
+
 				String zipCode = (splitAdd[1].trim().split("\\s+")[1]);
-				
 
-				HtmlDefinitionDescription webEmail = (HtmlDefinitionDescription) page.getByXPath("//div[@class='extra']//dl//dd").get(8);
+				HtmlDefinitionDescription webEmail = (HtmlDefinitionDescription) page
+						.getByXPath("//div[@class='extra']//dl//dd").get(8);
 				String email = (webEmail.getTextContent().split("\\s+")[0]);
-				
-				System.out.println(count + " people done");
-				count++;
-				
-				
-//				//listOfFakePeople.add(new Person (
-//								fName,
-//								lName,
-//								email,
-//								address,
-//								zipCode
-						//));
-				people.add(new Person (
-						fName,
-						lName,
-						email,
-						address,
-						zipCode
-				));
-				
-				Person target = new Person (
-						fName,
-						lName,
-						email,
-						address,
-						zipCode
-				);
-				
-				System.out.println(target);
-				
-				
-		        
-		        
-		        HtmlForm form = page.getForms().get(0);
-		        HtmlSubmitInput button = form.getInputByValue("Generate");
 
-		        try {
+				// System.out.println(count + " people done");
+				// count++;
+
+				listOfFakePeople.add(new Person(fName, lName, email, address, zipCode));
+				people.add(new Person(fName, lName, email, address, zipCode));
+
+				//Person target = new Person(fName, lName, email, address, zipCode);
+
+				// System.out.println(target);
+
+				HtmlForm form = page.getForms().get(0);
+				HtmlSubmitInput button = form.getInputByValue("Generate");
+
+				try {
 					page = button.click();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
@@ -124,18 +103,18 @@ public  class FakePeople extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		System.out.println(people);
-//		System.out.println(this.getName() + " is done");
+		// System.out.println(people);
+		// System.out.println(this.getName() + " is done");
 		this.interrupt();
-		
+
 	}
-	
-	public  String cutAdd(String s) {
-		if(s.length() > 2) {
+
+	public String cutAdd(String s) {
+		if (s.length() > 2) {
 			String r = "" + s.charAt(0);
 			int i = 1;
-			while(s.charAt(i) != Character.toUpperCase(s.charAt(i))) {
-				if(s.length() == i + 1)
+			while (s.charAt(i) != Character.toUpperCase(s.charAt(i))) {
+				if (s.length() == i + 1)
 					return s;
 				r += s.charAt(i);
 				i++;
@@ -145,15 +124,15 @@ public  class FakePeople extends Thread {
 			return s;
 		}
 	}
-	
+
 	public boolean containsUpp(String s) {
 		boolean r = false;
-		for(int i = 1; i < s.length(); i ++) {
-			if(s.charAt(i) == Character.toUpperCase(s.charAt(i))) {
+		for (int i = 1; i < s.length(); i++) {
+			if (s.charAt(i) == Character.toUpperCase(s.charAt(i))) {
 				return true;
 			}
 		}
 		return r;
-		
+
 	}
 }
--- src/thread.java
+++ src/thread.java
@@ -4,6 +4,8 @@ import java.io.*;
 
 public class thread {
 	public static void main(String[] args) {
+		long startTime = System.nanoTime();
+
 		// Remove Annoying Output
 		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF); 
 		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
@@ -29,11 +31,34 @@ public class thread {
 //				Person pers = new Person(lines[i]);
 //				pers.start();
 //			}
-			for(int i = 0; i < 100; i++) {
-				FakePeople pers = new FakePeople(3);
+			int processors = Runtime.getRuntime().availableProcessors();
+			System.out.println(processors + " Processors Available");
+			Thread[] threads = new Thread[processors]; 
+			for(int i = 0; i < processors; i++) {
+				FakePeople pers = new FakePeople(6);
+				threads[i] = pers;
 				pers.start();
 			}
 			
+			// Waits for all threads to finish
+			for(int i = 0; i < threads.length; i++)
+				try {
+					threads[i].join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+			System.out.println(FakePeople.listOfFakePeople);
+			
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			
+			long endTime = System.nanoTime();
+			System.out.println("Took "+(endTime - startTime) + " ns"); 
 			
 			
 //		} catch(FileNotFoundException e) {
